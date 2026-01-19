74aa7e8acd159c201d8ed0c71bd23b3f1b877b25
Ilkar5501 <questionarmorgames@gmail.com>
2026-01-19 11:47:46 -0500

    Add full character list

--- character.js
+++ character.js
@@ -1,12 +1,141 @@
 // characters.js
-module.exports = [
-  { key: "abdullah", name: "Abdullah", imageUrl: "https://media.discordapp.net/attachments/1459562950384685258/1462589395902267482/M_Abdullah.png?format=webp&quality=lossless&width=920&height=920" },
-  { key: "berserk", name: "Berserk", imageUrl: "https://media.discordapp.net/attachments/1459562950384685258/1462589396812304437/M_Berserk.png?format=webp&quality=lossless&width=920&height=920" },
-  { key: "blutsx", name: "Blutsx", imageUrl: "https://media.discordapp.net/attachments/1459562950384685258/1462589397257158757/M_Blutsx.png?format=webp&quality=lossless&width=920&height=920" },
-  { key: "cryselle", name: "Cryselle", imageUrl: "https://media.discordapp.net/attachments/1459562950384685258/1462589397768868039/M_Cryselle.jpg?format=webp&width=920&height=920" },
-  { key: "formal", name: "Formal", imageUrl: "https://media.discordapp.net/attachments/1459562950384685258/1462589398091563069/M_Formal.jpg?format=webp&width=750&height=750" },
-  { key: "james", name: "James", imageUrl: "https://media.discordapp.net/attachments/1459562950384685258/1462589398444019825/M_James.png?format=webp&quality=lossless&width=750&height=750" },
-  { key: "legit", name: "Legit", imageUrl: "https://media.discordapp.net/attachments/1459562950384685258/1462589398918107377/M_Legit.jpg?format=webp&width=750&height=750" },
-
-  // TODO: add the remaining characters here (we’ll do it after the system works)
+export default [
+  {
+    key: "abdullah",
+    name: "Abdullah",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589395902267482/M_Abdullah.png?ex=696ebe05&is=696d6c85&hm=60e01db49bc7380c75ea9bc9880cf47f3121f489003e0044fa050171d5193551&=&format=webp&quality=lossless&width=920&height=920",
+  },
+  {
+    key: "berserk",
+    name: "Berserk",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589396812304437/M_Berserk.png?ex=696ebe05&is=696d6c85&hm=7676656347777236196d5fb1251103704f7790e18ec0237fd8b52b6f17a450b8&=&format=webp&quality=lossless&width=920&height=920",
+  },
+  {
+    key: "blutsx",
+    name: "Blutsx",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589397257158757/M_Blutsx.png?ex=696ebe05&is=696d6c85&hm=563cce31cc66f7d95949390d6916216d499657d9833fd4f7ed9ddeb1d53f2791&=&format=webp&quality=lossless&width=920&height=920",
+  },
+  {
+    key: "cryselle",
+    name: "Cryselle",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589397768868039/M_Cryselle.jpg?ex=696ebe06&is=696d6c86&hm=134ef56128e4f655d13e0423a67218b6b8962899399a18f770782961dd633c46&=&format=webp&width=920&height=920",
+  },
+  {
+    key: "formal",
+    name: "Formal",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589398091563069/M_Formal.jpg?ex=696ebe06&is=696d6c86&hm=38c2ec21f81bdf80815f86e15ddb5be95745953618294c5fef7667ba0d1bc018&=&format=webp&width=750&height=750",
+  },
+  {
+    key: "james",
+    name: "James",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589398444019825/M_James.png?ex=696ebe06&is=696d6c86&hm=46261282262f579025f311627fccca591d8dcc897b2440c76a3d34f559185e6a&=&format=webp&quality=lossless&width=750&height=750",
+  },
+  {
+    key: "legit",
+    name: "Legit",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589398918107377/M_Legit.jpg?ex=696ebe06&is=696d6c86&hm=6c8586925a10efd3ec5e7fbb2332d7ca5d1a1fb427a771daa449ff7d7219caa0&=&format=webp&width=750&height=750",
+  },
+  {
+    key: "game_over_neos",
+    name: "Game Over & Neos",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589399198863563/M_Neos_And_GameOver.jpg?ex=696ebe06&is=696d6c86&hm=9f209fad865899105bcf773806b1ab3215ff90490d6cdcbc2fa020e13dcb4e6d&=&format=webp&width=750&height=750",
+  },
+  {
+    key: "poyo",
+    name: "Poyo",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589399857496196/M_Poyo.jpg?ex=696ebe06&is=696d6c86&hm=375bb08911d1a418e642b5874e6a870480e6f125bd6b731b3864c43991600a34&=&format=webp&width=750&height=750",
+  },
+  {
+    key: "renegade",
+    name: "Renegade",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589400234852483/M_Renegade.png?ex=696ebe06&is=696d6c86&hm=e9ee3b81e617fe961a6eede3960c9efcab41fd234e0633230bacc0e5dd372c13&=&format=webp&quality=lossless&width=625&height=625",
+  },
+  {
+    key: "rithy",
+    name: "Rithy",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589436532625571/M_Rithy.png?ex=696ebe0f&is=696d6c8f&hm=a9d9f3731e94829516369c8d78efca2d850f775b76db54fb986d22f195b12003&=&format=webp&quality=lossless&width=750&height=750",
+  },
+  {
+    key: "toe",
+    name: "Toe",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462593240489525436/M_Toe.jpg?ex=696ec19a&is=696d701a&hm=d087387d8e439ed2b954cc0c28156957c5678bdb4435ebbc93ad415e988e931d&=&format=webp&width=750&height=750",
+  },
+  {
+    key: "tubbs",
+    name: "Tubbs",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589437199253504/M_Tubbs.png?ex=696ebe0f&is=696d6c8f&hm=e202c32b6a19f5f8c98d5e5ee9543fe8a57224f0fa6fa0e1be4349bcf085ce28&=&format=webp&quality=lossless&width=625&height=625",
+  },
+  {
+    key: "wargrass",
+    name: "Wargrass",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589437643980800/M_Wargrass.png?ex=696ebe0f&is=696d6c8f&hm=816d56b4f083927f1e587a20acb61796c27c9cbb4fb4eb0b4c6a3f84d2f5cb6f&=&format=webp&quality=lossless&width=920&height=920",
+  },
+  {
+    key: "yaozers",
+    name: "Yaozers",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462593240896110592/M_Yaozers.jpg?ex=696ec19a&is=696d701a&hm=bf5f7b78d06781eb97a2ad80fae73e678557f315c09e563c6190e81d3b27f4ae&=&format=webp&width=750&height=750",
+  },
+  {
+    key: "zebulous",
+    name: "Zebulous",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589438948278313/M_Zebulous.jpg?ex=696ebe0f&is=696d6c8f&hm=5e22d12356c68bb376cc0f15ec01754c1a15814dd67edcd69dad02fcad41ec72&=&format=webp&width=920&height=920",
+  },
+  {
+    key: "azula",
+    name: "Azula",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589439413981184/W_Azula.jpg?ex=696ebe0f&is=696d6c8f&hm=15d80b9b70703e932d3374fc93c47b0aa1cb91ed917d6e7db6499f50b5f96cf2&=&format=webp&width=875&height=875",
+  },
+  {
+    key: "lala",
+    name: "Lala",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589439766433843/W_Lala.png?ex=696ebe10&is=696d6c90&hm=d3094a3bbf5b66c84b6773d2697f5aa6830a8626c5d9f57d9c860b080d6d096e&=&format=webp&quality=lossless&width=750&height=750",
+  },
+  {
+    key: "shmeargle",
+    name: "Shmeargle",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589440336597126/W_Shmeargle.png?ex=696ebe10&is=696d6c90&hm=7b7d08dee30c0697fa9500ba880e1244b1490343f5f5288e13b695499222c45a&=&format=webp&quality=lossless&width=920&height=920",
+  },
+  {
+    key: "starolyz",
+    name: "Starolyz",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589440785649844/W_Starolyz.jpg?ex=696ebe10&is=696d6c90&hm=5f287eb627982908bb71d378fb4d71950d4884b8a752159976ed37d9e5e7a6ce&=&format=webp&width=920&height=920",
+  },
+  {
+    key: "stitch",
+    name: "Stitch",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589455998124167/W_Stitch.png?ex=696ebe13&is=696d6c93&hm=bda8504d9f428d68e7c4429b87c7eae4187b1d6d1390b935e5906c820c6252cb&=&format=webp&quality=lossless&width=920&height=920",
+  },
+  {
+    key: "wabbit",
+    name: "Wabbit",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589456304570652/W_Wabbit.jpg?ex=696ebe13&is=696d6c93&hm=893d212bd271e03a17bd6fd45ff03a74c4a98a85aba4fc5e2d4164a69010e311&=&format=webp&width=920&height=920",
+  },
+  {
+    key: "woobi",
+    name: "Woobi",
+    imageUrl:
+      "https://media.discordapp.net/attachments/1459562950384685258/1462589456660828261/W_Woobi.png?ex=696ebe14&is=696d6c94&hm=26de542d277f4ee0eabc22d4327be3f151a2e16031920071ac05fc74d896c92e&=&format=webp&quality=lossless&width=920&height=920",
+  },
 ];
